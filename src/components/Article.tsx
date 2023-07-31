455520d926878ae39d979533f0445124b43dfe38
gustavocodigo <gilapesgus@gmail.com>
2023-07-31 14:16:46 -0300

    fixed - hrefs click

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -10,14 +10,18 @@ interface ArticleProps {
 
 export const Article = ({ imageData, alt = "", title, content, textFirst = false }: ArticleProps) => {
   return (
-    <article
-      className={`max-w-full flex ${textFirst ? 'flex-row-reverse' : ''} w-full m-auto max-w-full flex-wrap md:flex-nowrap justify-between md:gap-16`}
-    >
-      <Image className="max-w-full w-200 md:w-50" src={imageData} alt={alt} width={200} height={200} />
-      <div className="w-full">
-        <h2 className="">{title}</h2>
-        <p className="md:w-600">{content}</p>
-      </div>
-    </article>
+
+    <div className='lg:min-h-[40vh] max-h-[700px] flex justify-center items-center max-w-full'>
+      <article
+        className={`max-w-full flex ${textFirst ? 'flex-row-reverse' : ''} w-full m-auto max-w-full flex-wrap md:flex-nowrap justify-between md:gap-16`}
+      >
+        <Image className="max-w-full w-200 md:w-50" src={imageData} alt={alt} width={200} height={200} />
+        <div className="w-full mt-12 max-w-full">
+          <h2 className="">{title}</h2>
+          <p className="max-w-full">{content}</p>
+        </div>
+      </article>
+    </div>
+
   )
 }
--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,68 +1,80 @@
 import { useState } from "react"; // import state
 
 export default function Header(props: any) {
-    const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
 
-    return (
-        <div className={"flex items-center justify-between border-b border-gray-400 " + props.className}>
-            <nav>
-                <section className="MOBILE-MENU flex lg:hidden">
-                    <div
-                        className="HAMBURGER-ICON space-y-2"
-                        onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
-                    >
-                        <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                        <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                        <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                    </div>
+  return (
+    <div className={"flex items-center justify-between border-b border-gray-400 " + props.className}>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+          </div>
 
-                    <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-                        <div
-                            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-                            onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
-                        >
-                            <svg
-                                className="h-8 w-8 text-gray-600"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <line x1="18" y1="6" x2="6" y2="18" />
-                                <line x1="6" y1="6" x2="18" y2="18" />
-                            </svg>
-                        </div>
-                        <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-                            {props.children}
-                        </ul>
-                    </div>
-                </section>
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            >
+              <svg
+                className="h-8 w-8 text-white"
+                viewBox="0 0 60 60"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="60" y1="12" x2="12" y2="60" />
+                <line x1="12" y1="12" x2="60" y2="60" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] ">
+              {props.children}
+            </ul>
+          </div>
+        </section>
 
-                <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
-                    {props.children}
-                </ul>
-            </nav>
-            <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: #000000c9;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-        </div>
-    );
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+          {props.children}
+        </ul>
+      </nav>
+      <style>{`
+        .hideMenuNav {
+          display: block;
+          opacity: 0;
+          position: absolute;
+         
+          width: 100%;
+          height: 100vh;
+          visibility: hidden;
+          transition: opacity 0.5s, visibility 0.5s;
+          z-index: 10;
+        }
+
+        .showMenuNav {
+          display: block;
+          opacity: 1;
+          visibility: visible;
+          transition: opacity 0.5s, visibility 0.5s;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: #000000c9;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  );
 }
